3b9a9914a4e29409c8d082d3af25b8d119caa9f2
dpqlsn <a01037111209@gmail.com>
2025-12-03 01:22:49 +0900

    feat :: 백그라운드 이미지 추가

--- src/container/home/index.tsx
+++ src/container/home/index.tsx
@@ -1,9 +1,10 @@
 import * as _ from './style';
+import Image from 'next/image';
 
 export default function Home() {
     return (
         <_.Contaienr>
-            Home Container
+            <Image src="/BG.gif" alt="background image" width={2000} height={1080} style={{ overflowY: 'auto', opacity: 0.5 }} />
         </_.Contaienr>
     );
 }
