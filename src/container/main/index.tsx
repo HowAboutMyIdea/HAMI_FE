0ba754de54263d82ae82840e223eb78c8e55054b
dpqlsn <a01037111209@gmail.com>
2025-12-11 08:11:32 +0900

    feat :: Enter 키 클릭 이벤트 추가

--- src/container/main/index.tsx
+++ src/container/main/index.tsx
@@ -1,15 +1,30 @@
 "use client";
 
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import * as S from './style';
 
-export default function Landing() {
+export default function Main() {
+    const router = useRouter();
+    const [idea, setIdea] = useState('');
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            router.push('/');
+        }
+    };
 
     return (
-        <S.Contaienr>
+        <S.Container>
             <S.TextGroup>
                 <S.Title>아이디어를 적어보세요</S.Title>
-                <S.SubTitle placeholder="아이디어를 적은 후 Enter를 눌러주세요" />
+                <S.SubTitle
+                    placeholder="아이디어를 적은 후 Enter를 눌러주세요"
+                    value={idea}
+                    onChange={(e) => setIdea(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                />
             </S.TextGroup>
-        </S.Contaienr>
+        </S.Container>
     );
 }
