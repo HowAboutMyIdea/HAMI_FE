f0054d9e3e093e9aecd3c9eef12e209c7c05665c
dpqlsn <a01037111209@gmail.com>
2025-12-12 08:20:47 +0900

    feat :: 백엔드 연동

--- src/service/index.tsx
+++ src/service/index.tsx
@@ -1,6 +1,17 @@
 import customAxios from "@/lib/customAxios";
 
-export const upsertNickname = async () => {
-    const response = await customAxios.post("/idea");
+export type ExtractedIdeaResponse = {
+    main_subject: string;
+    keywords: string[];
+    summary: string;
+};
+
+export const upsertNickname = async (payload?: { nickname?: string; idea?: string }) => {
+    const response = await customAxios.post("/idea", payload ?? {});
+    return response.data;
+};
+
+export const extractIdea = async (text: string) => {
+    const response = await customAxios.post<ExtractedIdeaResponse>("/idea", { text });
     return response.data;
 };
