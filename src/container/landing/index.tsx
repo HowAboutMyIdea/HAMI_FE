51ff2d75dbba952a85963a3146efab6ccbd55cfd
dpqlsn <a01037111209@gmail.com>
2026-01-13 00:35:19 +0900

    perf :: 오타수정

--- src/container/landing/index.tsx
+++ src/container/landing/index.tsx
@@ -16,12 +16,12 @@ export default function Landing() {
     }, [router]);
 
     return (
-        <S.Contaienr onClick={() => router.push('/main')}>
+        <S.Container onClick={() => router.push('/main')}>
             <S.TextGroup>
                 <S.Title>당신의 아이디어를</S.Title>
                 <S.Title>시험해보세요</S.Title>
                 <S.SubTitle>화면을 누르면 이동합니다</S.SubTitle>
             </S.TextGroup>
-        </S.Contaienr>
+        </S.Container>
     );
 }
--- src/container/landing/style.ts
+++ src/container/landing/style.ts
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 
-export const Contaienr = styled.div`
+export const Container = styled.div`
   width: 100%;
   height: 100vh;
   background-image: url("/BG.gif");
